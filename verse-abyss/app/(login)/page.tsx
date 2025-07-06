b09516a2a4484d105f1f242e5feeea6a20165973
PedroOliveiraWish <pedrowebdeveloper.oliveira@gmail.com>
2025-07-05 22:10:29 -0300

    FEAT: Estilizando a página de login utilizando scss

--- verse-abyss/app/(login)/page.tsx
+++ verse-abyss/app/(login)/page.tsx
@@ -7,6 +7,8 @@ import { zodResolver } from '@hookform/resolvers/zod'
 
 import { authLogin, authSignup } from '@/pages/api/auth'
 
+import './login.css'
+
 // Definindo os schemas de login e cadastro
 
 const CadastroSchema = z.object({
@@ -44,20 +46,20 @@ function Login({ onClick }: { onClick: (state: boolean) => void }) {
 
     const onSubmit = async (data: LoginType) => {
         setIsLoading(true)
+        setApiError("")
         try {
-            const response = await authLogin(data.email, data.password, "http://localhost:3000/auth/login");
-
+            const response = await authLogin(data.email, data.password, "http://localhost:3001/auth/login");
 
             const json = await response.json()
 
             if (!response.ok) {
-                setApiError(json.message)
+                return setApiError(json?.message || "Erro ao fazer login. Tente novamente.");
             }
 
-            console.log(json)
+            localStorage.setItem('token', json.userToken.token)
         } catch (err) {
             console.log(err)
-            setApiError("Erro ao conectar com o servidor.")
+            setApiError((prev) => prev || "Erro ao conectar com o servidor.")
         } finally {
             setIsLoading(false)
         }
@@ -65,20 +67,20 @@ function Login({ onClick }: { onClick: (state: boolean) => void }) {
 
     return (
         <section className='login'>
-            <div className="title-auth">Entre</div>
+            <div className="title-auth">Acesse seu jardim espiritual</div>
 
-            {apiError && <p>{apiError}</p>}
+            {apiError && <p className='api-error'>{apiError}</p>}
 
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="group">
                     <label>Email</label>
                     <input {...register('email')} />
-                    {errors.email && <p>{errors.email.message}</p>}
+                    {errors.email && <p className='zod-error'>{errors.email.message}</p>}
                 </div>
                 <div className="group">
                     <label>Senha</label>
                     <input type='password' {...register('password')} />
-                    {errors.password && <p>{errors.password.message}</p>}
+                    {errors.password && <p className='zod-error'>{errors.password.message}</p>}
                 </div>
                 <div className="button-submit">
                     <button type="submit" disabled={isLoading}>
@@ -86,7 +88,7 @@ function Login({ onClick }: { onClick: (state: boolean) => void }) {
                     </button>
                 </div>
             </form>
-            <p>
+            <p className='update-state'>
                 <span>Ainda não tem cadastro? </span>
                 <button onClick={() => onClick(false)}>cadastre-se</button>
             </p>
@@ -105,14 +107,15 @@ function Cadastro({ onClick }: { onClick: (state: boolean) => void }) {
 
     const onSubmit = async (data: CadastroType) => {
         setIsLoading(true)
+        setApiError("")
 
         try {
-            const response = await authSignup(data.nome, data.email, data.password, "http://localhost:3000/auth/register");
+            const response = await authSignup(data.nome, data.email, data.password, "http://localhost:3001/auth/register");
 
             const json = await response.json();
 
             if (!response.ok) {
-                setApiError(json.message)
+                return setApiError(json?.message || "Erro ao fazer cadastro. Tente novamente.");
             }
 
             if (response.ok) {
@@ -122,7 +125,7 @@ function Cadastro({ onClick }: { onClick: (state: boolean) => void }) {
             console.log(json)
         } catch (err) {
             console.log(err)
-            setApiError("Erro ao conectar com o servidor.")
+            setApiError((prev) => prev || "Erro ao conectar com o servidor.")
         } finally {
             setIsLoading(false)
         }
@@ -132,37 +135,37 @@ function Cadastro({ onClick }: { onClick: (state: boolean) => void }) {
         <section className='cadastro'>
             <div className="title-auth">Cadastre-se</div>
 
-            {apiError && <p>{apiError}</p>}
+            {apiError && <p className='api-error'>{apiError}</p>}
 
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="group">
                     <label>Nome</label>
                     <input {...register('nome')} />
-                    {errors.nome && <p>{errors.nome.message}</p>}
+                    {errors.nome && <p className='zod-error'>{errors.nome.message}</p>}
                 </div>
                 <div className="group">
                     <label>Email</label>
                     <input {...register('email')} />
-                    {errors.email && <p>{errors.email.message}</p>}
+                    {errors.email && <p className='zod-error'>{errors.email.message}</p>}
                 </div>
                 <div className="group">
                     <label>Senha</label>
                     <input type='password' {...register('password')} />
-                    {errors.password && <p>{errors.password.message}</p>}
+                    {errors.password && <p className='zod-error'>{errors.password.message}</p>}
                 </div>
                 <div className="group">
                     <label>Confirmar senha</label>
                     <input type='password' {...register('confirmPassword')} />
-                    {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+                    {errors.confirmPassword && <p className='zod-error'>{errors.confirmPassword.message}</p>}
                 </div>
 
                 <div className="button-submit">
-                    <button type="submit">
+                    <button type="submit" disabled={isLoading}>
                         {isLoading ? "Cadastrando..." : "Cadastrar"}
                     </button>
                 </div>
             </form>
-            <p>
+            <p className='update-state'>
                 <span>Já tem cadastro? </span>
                 <button onClick={() => onClick(true)}>faça login</button>
             </p>
@@ -175,11 +178,24 @@ export default function AuthPage() {
 
     return (
         <div className="container-auth">
+            <div className="welcome">
+                <div className="title-auth-container">verse abyss</div>
+
+                <div className="auth-message">
+                    {isLogin ? (
+                        <p>✨ O Abismo sussurra teu nome. Bem-vinda de volta à escuridão sagrada, onde os ecos infinitos da alma encontram seu lar. Que tua mente se perca e se reencontre aqui.</p>
+                    ) : (
+                        <p>🌒 Adentre as profundezas. Que tua alma encontre um novo reflexo nos espelhos do Verse Abyss. Sinta a vastidão e a quietude que aguardam por ti.</p>
+                    )}
+                </div>
+            </div>
+
             {isLogin ? (
                 <Login onClick={setIsLogin} />
             ) : (
                 <Cadastro onClick={setIsLogin} />
             )}
         </div>
+
     )
 }
